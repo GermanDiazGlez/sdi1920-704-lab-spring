28f7d7c33ea14aca2305209db89d81af54b44227
UO258472 <uo258472@uniovi.es>
2020-02-12 17:24:29 +0100

    SDI - 1.8 Trabajando con Controladores.

--- sdi1920-704-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi1920-704-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,6 +1,10 @@
 package com.uniovi.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Mark;
 
